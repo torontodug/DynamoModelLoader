e179caf04e3faa51f66720c54acaa8e096cc274b
Mohsen A <mohs.a437@gmail.com>
2020-03-20 21:19:44 -0400

    Fixes and Added Support for OBJ Group

--- DynModelLoader/src/ModelLoader.cs
+++ DynModelLoader/src/ModelLoader.cs
@@ -6,8 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Autodesk.DesignScript.Geometry;
 using Autodesk.DesignScript.Runtime;
-using DynamoUnits;
-
+using ObjLoader.Loader.Data.Elements;
 using ObjLoader.Loader.Loaders;
 
 namespace DynModelLoader
@@ -21,36 +20,42 @@ namespace DynModelLoader
         /// Loads geometry from an OBJ file into native Dynamo geometry.
         /// </summary>
         /// <param name="filePath">The filepath of the .OBJ file.</param>
-        /// <returns></returns>
-        //[MultiReturn(new[] { "Indices", "Points"})]
-        public static PolySurface LoadObj(string filePath)
+        /// <returns>List of OBJ Groups as Dynamo PolySurfaces.</returns>
+        public static List<PolySurface> LoadObj(string filePath)
         {
             var loaderFactory = new ObjLoaderFactory();
             var loader = loaderFactory.Create();
-            
 
             var fs = new FileStream(filePath, FileMode.Open);
             var result = loader.Load(fs);
             
             fs.Close();
 
-            var polygons = new List<Surface>();
-            var points = new List<Point>();
+            List<PolySurface> polySurfaces = new List<PolySurface>();
 
             foreach(var group in result.Groups)
             {
-                foreach(var face in group.Faces)
+                polySurfaces.Add(GetObjGroupMesh(group, result));
+            }
+
+            return polySurfaces;
+        }
+
+        private static PolySurface GetObjGroupMesh(Group group, LoadResult result)
+        {
+            var polygons = new List<Surface>();
+            var points = new List<Point>();
+
+            foreach (var face in group.Faces)
+            {
+                for (int i = 0; i < face.Count; i++)
                 {
-                    for (int i = 0; i < face.Count; i++)
-                    {
-                        var index = face[i].VertexIndex - 1;
-                        points.Add(Point.ByCoordinates(result.Vertices[index].X, result.Vertices[index].Y, result.Vertices[index].Z));
-                    }
-                    
+                    var index = face[i].VertexIndex - 1;
+                    points.Add(Point.ByCoordinates(result.Vertices[index].X, result.Vertices[index].Y, result.Vertices[index].Z));
                 }
             }
 
-            for (int i = 0; i < points.Count; i+=3)
+            for (int i = 0; i < points.Count; i += 3)
             {
                 int j = i + 1;
                 int k = i + 2;
@@ -59,8 +64,7 @@ namespace DynModelLoader
                 var point2 = points[j];
                 var point3 = points[k];
 
-
-                var facePoints = new List<Point>() { point1, point2, point3};
+                var facePoints = new List<Point>() { point1, point2, point3 };
                 try
                 {
                     polygons.Add(Surface.ByPerimeterPoints(facePoints));
@@ -76,6 +80,7 @@ namespace DynModelLoader
                     continue;
                 }
             }
+
             return PolySurface.ByJoinedSurfaces(polygons);
         }
     }
